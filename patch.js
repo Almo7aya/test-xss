b7329a3c02827bcd08cc425e33592cd8ca7ec4b5
Ali Almohaya <alialmohaya@gmail.com>
2024-09-08 10:31:51 +0300

    chore: add more code

--- patch.js
+++ patch.js
@@ -1,6 +1,12 @@
+if (window.loaded) return;
+
+window.loaded = true;
+const xss = document.createElement('div');
+xss.innerText = 'XSS';
+document.querySelector('.rocket_con').appendChild(xss);
+
 // Save the original JSON.parse method
 const originalJSONParse = JSON.parse;
-
 console.log("loading the xss script");
 // Create a new parse method
 JSON.parse = function (text, reviver) {
